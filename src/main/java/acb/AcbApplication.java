d0ec15c1ef9f6e431da336f30c6c7d51f73760b1
namson0482@gmail.com <namson0482@gmail.com>
2021-06-29 15:54:17 +0700

    initial source codes

---
+++ src/main/java/acb/AcbApplication.java
@@ -0,0 +1,29 @@
+package acb;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@SpringBootApplication
+public class AcbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcbApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
+     return new OpenAPI()
+          .info(new Info()
+          .title("acb application API")
+          .version(appVersion)
+          .description(appDesciption)
+          .termsOfService("http://swagger.io/terms/")
+          .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
